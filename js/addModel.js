83006d38e6387d595a4459fd105c2ccf38a0f4c6
mexx91 <maximiliano_gross@yahoo.de>
2013-11-21 15:58:16 +0100

    Update addModel.js

--- js/addModel.js
+++ js/addModel.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     var mouseX, mouseY;
     var targetList = [];
@@ -9,7 +8,7 @@ $(document).ready(function () {
 
         event.preventDefault();
 
-        var vector = new THREE.Vector3(  (event.clientX / window.innerWidth ), - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+        var vector = new THREE.Vector3(  (event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
         projector.unprojectVector( vector, camera );
 
         var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
